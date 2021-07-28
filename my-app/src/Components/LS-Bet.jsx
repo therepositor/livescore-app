5299999fbb739d7b867bb47123b9b52e37ea4374
Joseph Oguntebi <joseph.oguntebi@yahoo.com>
2021-07-28 11:26:40 +0100

    added footer, More page and Livescore bet

---
+++ my-app/src/Components/LS-Bet.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const LiveScoreBet = () =>  {
+
+    return (
+        <div className='bet-container'>
+            <p>Bet is not available right now</p>
+            <footer>
+                <p>&copy; 1996 - 2021 LiveScore</p>
+                <div className='footer-navlinks'>
+                    <a href="careers.html">Careers</a> | 
+                    <a href="mobile.html">Mobile</a> | 
+                    <a href="advertise.html">Advertise</a> | 
+                    <a href="contact.html">Contact</a> | 
+                    <a href="news-publishers.html">News Publishers</a> | 
+                    <a href="modern-slavery-act.html">Modern Slavery Act</a> | 
+                    <a href="privacy-notice.html">Privacy Notice</a> | 
+                    <a href="cookie-policy.html">Cookie Policy</a> | 
+                    <a href="terms-of-use.html">Terms of Use</a> | 
+                    <a href="corporate.html">Corporate</a>
+                </div>
+            </footer>
+        </div>
+    )
+}
+export default LiveScoreBet
--- my-app/src/Routes/index.jsx
+++ my-app/src/Routes/index.jsx
@@ -4,6 +4,8 @@ import Home from '../Components/Home';
 import Scorelist from '../Components/Scores';
 import News from '../Components/News';
 import MenuPage from '../Components/MenuPage.jsx';
+import More from '../Components/More.jsx';
+import LiveScoreBet from '../Components/LS-Bet.jsx';
 
 
 const Routes = () => (
@@ -14,6 +16,8 @@ const Routes = () => (
             <Route path='/scores' exact component={Scorelist} /> 
             <Route path='/news' exact component={News} />
             <Route path='/menu' exact component={MenuPage} />
+            <Route path='/more' exact component={More} />
+            <Route path='/ls_bet' exact component={LiveScoreBet} />
         </Switch>
     </BrowserRouter>
 )
